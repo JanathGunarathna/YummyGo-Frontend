ce3c955793c25386c0947997192f154b20d7d157
janath <janathmdrng@gmail.com>
2025-10-27 21:59:04 +0530

    chnaged

--- adminPanel/src/App.jsx
+++ adminPanel/src/App.jsx
@@ -1,23 +1,39 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AddFood from './pages/AddFood/AddFood.jsx';
+import ListFood from './pages/ListFoods/ListFood.jsx';
+import Orders from './pages/Orders/Orders.jsx';
+import Sidebar from './components/Sidebar/Sidebar.jsx';
+import Menubar from './components/Menubar/Menubar.jsx';
 
 const App = () => {
-  return (
-    <div className="d-flex" id="wrapper">
-            <Sidebar />
-            <div id="page-content-wrapper">
-                <Menubar />
-                <div className="container-fluid">
-                    <Route>
-                      <Route path='/add' element={<AddFood/>}/>
-                      <Route path='/list' element={<ListFood/>}/>
-                      <Route path='/orders' element={<Orders/>}/>
-                      <Route path='/' element={<ListFood/>}/>
-                    </Route>
-                </div>
-            </div>
+  const [sidebarVisible, setSidebarVisible] = useState(true);
+
+  const toggleSidebar = () => {
+    setSidebarVisible(!sidebarVisible);
+  }
+  return  (
+    <Router>
+      <div className="d-flex" id="wrapper">
+
+        <Sidebar sidebarVisible={sidebarVisible} />
+
+        <div id="page-content-wrapper">
+
+          <Menubar toggleSidebar={toggleSidebar}/>
+
+          <div className="container-fluid">
+            <Routes>
+              <Route path="/add" element={<AddFood />} />
+              <Route path="/list" element={<ListFood />} />
+              <Route path="/orders" element={<Orders />} />
+              <Route path="/" element={<ListFood />} />
+            </Routes>
+          </div>
         </div>
-  )
-}
+      </div>
+    </Router>
+  );
+};
 
 export default App;
